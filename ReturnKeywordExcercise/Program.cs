9f074377eb4e09ded67dbf21d73ca71b276affa8
Kristian Lubina <kristian.lubina@outlook.com>
2025-03-22 22:54:11 +0100

    Initial commit

---
+++ ReturnKeywordExcercise/Program.cs
@@ -0,0 +1,21 @@
+﻿//I have replciated the code from the following yt-tutorial for excersise purposes: https://www.youtube.com/watch?v=kxxsFmWU8YU
+
+namespace ReturnKeywordExcercise;
+
+class Program
+  {
+  static void Main( string[] args )
+    {
+    int cubedNumber = Cube( 5 );
+    //responsible for the visualization of the result of the calculation
+    Console.Write( cubedNumber );
+    Console.ReadLine();
+    }
+  static int Cube( int num )
+    {
+    //the calculation-result gets intialized
+    int result = num * num * num;
+    //after the calculation is finished the result gets returned bakc tot he caller-method of Cube() which is Main()
+    return result;
+    }
+  }
